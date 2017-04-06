db7e682eac5c4cb1be7dc2766068a0df5a33e588
Dominika <kumkwaczka@gmail.com>
2017-04-06 22:17:45 +0200

    fixing slider and adding static pages

---
+++ purrlove/js/categories.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        slideBy: 1,
+		autoplay: true,
+        dots: true,
+        loop: true
+    });
+});
