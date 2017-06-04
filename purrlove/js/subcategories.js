dc73c63663609a20c5b57d974054dd362985d061
Dominika <kumkwaczka@gmail.com>
2017-06-04 14:30:13 +0200

    adding images and subpages

---
+++ purrlove/js/subcategories.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoplay: false,
+        dots: false,
+        nav: true,
+        loop: true,
+        items: 1,
+        slideBy: 1
+
+    });
+     $(".owl-prev").html('<img src="img/arrow_left.png">');
+    $(".owl-next").html('<img src="img/arrow_right.png">');
+});
