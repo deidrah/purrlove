c7915840c90748d17a58a45cd0edc14a07ac3ea2
Dominika <kumkwaczka@gmail.com>
2017-06-04 22:24:30 +0200

    adding part of the responsiveness

--- purrlove/js/main.js
+++ purrlove/js/main.js
@@ -3,22 +3,11 @@ $(document).ready(function () {
         autoplay: true,
         dots: true,
         loop: true,
-        responsive: {
-            0: {
-                items: 1,
-                slideBy: 1
-            },
-            850: {
-                items: 2,
-                slideBy: 2
-            },
-            1300: {
-                items: 3,
-                slideBy: 3
-            }
-        }
+        items: 3,
+        slideBy: 3
+
     });
-    $('.item').click(function() {
-        $(this).find('.item-cart').html('<span>W koszyku</span>');          
+    $('.item').click(function () {
+        $(this).find('.item-cart').html('<span>W koszyku</span>');
     });
 });
