9e1f619774ccacccf95136917d9d49d7b463d40f
Dominika <kumkwaczka@gmail.com>
2017-06-04 17:54:08 +0200

    adding checkout

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function () {
-    return gulp.src('purrlove/scss/base.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('purrlove/css'))
+gulp.task('sass', function(){
+  return gulp.src('purrlove/scss/base.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false
+        }))
+    .pipe(gulp.dest('purrlove/css'))
+});
+
+gulp.task('default', function() {
+    gulp.watch(['purrlove/scss/*.scss'], ['sass'])
 });
