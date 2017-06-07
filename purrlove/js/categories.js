4be6ae0a951e8ed7b9a2c60baeac2c2d907beb2d
deidrah <dominika@iMac-Dominika.local>
2017-06-07 13:54:50 +0200

    small fix

--- purrlove/js/categories.js
+++ purrlove/js/categories.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        slideBy: 1,
-		autoplay: true,
-        dots: true,
-        loop: true
-    });
     $('.item').click(function() {
         $(this).find('.item-cart').html('<span>W koszyku</span>');          
     });
