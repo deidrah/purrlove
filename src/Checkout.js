eb5467865bb0b8de2cf0ea1e8703a552602cfdc9
deidrah <kumkwaczka@gmail.com>
2020-07-15 18:17:03 +0200

    Convert checkout page to react

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Purrlove - Checkout</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/base.css">
-</head>
+import React from 'react';
 
-<body>
-    <div class="container">
+function Checkout() {
+  return (
+<div class="container">
         <header>
             <nav class="nav-col">
                 <ul>
@@ -27,7 +21,7 @@
             <div class="container">
                 <div class="checkout-row">
                     <div class="row-name">
-                        <img src="img/item-fruity.png" alt="">
+                        <img src="img/item-fruity.png" alt="" />
                         <p>Fruity</p>
                     </div>
                     <div class="row-info">
@@ -37,7 +31,7 @@
                 </div>
                 <div class="checkout-row">
                     <div class="row-name">
-                        <img src="img/item-flamingo.png" alt="">
+                        <img src="img/item-flamingo.png" alt="" />
                         <p>Flamingo</p>
                     </div>
                     <div class="row-info">
@@ -47,7 +41,7 @@
                 </div>
                 <div class="checkout-row">
                     <div class="row-name">
-                        <img src="img/item-galaxy.png" alt="">
+                        <img src="img/item-galaxy.png" alt="" />
                         <p>Galaxy</p>
                     </div>
                     <div class="row-info">
@@ -57,7 +51,7 @@
                 </div>
                 <div class="checkout-row">
                     <div class="row-name">
-                        <img src="img/item-herbs.png" alt="">
+                        <img src="img/item-herbs.png" alt="" />
                         <p>Subtle herbs</p>
                     </div>
                     <div class="row-info">
@@ -69,20 +63,20 @@
                 <div class="checkout-data">
                    <h2>Adres dostawy</h2>
                     <form id="checkout-address" method="get">
-                        <input type="text" placeholder="Imię i Nazwisko">
-                    <input type="text" placeholder="Ulica oraz nr domu">
-                    <input type="text" placeholder="Kod pocztowy i miasto">
+                        <input type="text" placeholder="Imię i Nazwisko" />
+                    <input type="text" placeholder="Ulica oraz nr domu" />
+                    <input type="text" placeholder="Kod pocztowy i miasto" />
                     </form>
                 </div>
                 <div class="checkout-delivery">
                     <h2>Dostawa</h2>
-                    <input type="radio" name="delivery" id="delivery-one"><label for="delivery-one">Za pobraniem - 15zł</label>
-                    <input type="radio" name="delivery" id="delivery-two"><label for="delivery-two">Przedpłata - 10zł</label>
-                    <input type="radio" name="delivery" id="delivery-three"><label for="delivery-three">Odbiór osobisty - 0zł</label>
+                    <input type="radio" name="delivery" id="delivery-one" /><label for="delivery-one">Za pobraniem - 15zł</label>
+                    <input type="radio" name="delivery" id="delivery-two" /><label for="delivery-two">Przedpłata - 10zł</label>
+                    <input type="radio" name="delivery" id="delivery-three" /><label for="delivery-three">Odbiór osobisty - 0zł</label>
                 </div>
                 <div class="checkout-sumup">
                     <p>SUMA: 100zł</p>
-                    <input type="submit" value="Zamów">
+                    <input type="submit" value="Zamów" />
                 </div>
             </div>
         </div>
@@ -99,9 +93,7 @@
             </div>
         </footer>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-</body>
+  );
+}
 
-</html>
+export default Checkout;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { Route } from "wouter";
 import Home from './Home';
 import About from './About';
 import Categories from './Categories';
+import Checkout from './Checkout';
 
 import './scss/base.scss';
 
 const App = () => (
   <div>
+    <Route path="/checkout"><Checkout /></Route>
     <Route path="/categories"><Categories /></Route>
     <Route path="/about"><About /></Route>
     <Route path="/"><Home /></Route>
