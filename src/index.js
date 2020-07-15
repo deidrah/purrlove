ed3817e8a952d24ac0064552f0437bbc502716db
deidrah <kumkwaczka@gmail.com>
2020-07-15 17:44:11 +0200

    Use scss file in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './css/base.css';
-import './css/owl.carousel.min.css';
-import './css/owl.theme.default.min.css';
+import './scss/base.scss';
 
 ReactDOM.render(
   <App />,
