9c859e77185d8e25504d3a4366aa079f1326b650
deidrah <kumkwaczka@gmail.com>
2020-07-15 18:07:44 +0200

    Add router to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from "wouter";
 import Home from './Home';
 
 import './scss/base.scss';
 
+const App = () => (
+  <div>
+    <Route path="/"><Home /></Route>
+  </div>
+);
+
 ReactDOM.render(
-  <Home />,
+  <App />,
   document.getElementById('root')
 );
