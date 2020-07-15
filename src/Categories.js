51eb897578422283df2e1af4bc651f1830218090
deidrah <kumkwaczka@gmail.com>
2020-07-15 18:15:05 +0200

    Convert categories page to react

---
+++ src/Categories.js
@@ -0,0 +1,181 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div class="container">
+    <header>
+        <nav class="nav-col">
+            <ul>
+                <li><a href="subpages.html">Kim jesteśmy?</a></li>
+                <li><a href="categories.html">Legowiska</a></li>
+            </ul>
+        </nav>
+        <a href="index.html">
+            <div id="logo" class="nav-col">
+            </div>
+        </a>
+        <div id="actions" class="nav-col">
+            <a href="checkout.html">Twoje produkty: <div id="cart">4</div></a>
+        </div>
+    </header>
+    <div id="cat-hero">
+        <div class="slider-col slider-container">
+            <div class="slider-name"></div>
+            <p>Ut enim ad minim veniam,<br />quis nostrud </p>
+            <p class="prize">79.90<span>PLN</span></p>
+            <button>Zobacz</button>
+        </div>
+    </div>
+    <div id="catalogue">
+        <div id="catalogue-nav">
+            <div id="cat">
+                <h4>Kategorie</h4>
+                <button>usuń filtry</button>
+                <ul>
+                    <li><a href="#" id="active-paw">Kwiatowe</a></li>
+                    <li><a href="#">Vintage</a></li>
+                    <li><a href="#">Geometryczne</a></li>
+                    <li><a href="#">Marinistyczne</a></li>
+                    <li><a href="#">Kocie</a></li>
+                    <li><a href="#">Malarskie</a></li>
+                </ul>
+            </div>
+            <div id="models">
+                <h4>Modele</h4>
+                <ul>
+                    <li><a href="#">Koło</a></li>
+                    <li><a href="#">Chatka</a></li>
+                    <li><a href="#">Kwadrat</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="cat-items">
+            <div id="cat-filters">
+                <h4>Sortuj</h4>
+                <ul>
+                    <li><a href="#">Cena rosnąco</a></li>
+                    <li><a href="#">Cena malejącco</a></li>
+                    <li><a href="#">Nazwa</a></li>
+                    <li><a href="#">Popuplarność</a></li>
+                </ul>
+            </div>
+            <div id="product-cat-container">
+                <div class="item-col">
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-one">
+                        </div>
+                        <h3>Tropicana</h3>
+                        <span class="model">Model : Koło</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-two"></div>
+                        <h3>Vintage Floral</h3>
+                        <span class="model">Model : Chatka</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-three">
+                        </div>
+                        <h3>Morska Piana</h3>
+                        <span class="model">Model : Kwadrat</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                </div>
+                <div class="item-col">
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-four">
+                        </div>
+                        <h3>Geometric</h3>
+                        <span class="model">Model : Koło</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-five"></div>
+                        <h3>Subtle Herbs</h3>
+                        <span class="model">Model : Koło</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-six">
+                        </div>
+                        <h3>Fruity</h3>
+                        <span class="model">Model : Kwadrat</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                </div>
+                <div class="item-col">
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-seven">
+                        </div>
+                        <h3>Flamingo</h3>
+                        <span class="model">Model : Koło</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-eight"></div>
+                        <h3>Darkblue floral</h3>
+                        <span class="model">Model : Koło</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                    <div class="item">
+                        <div class="action-item">
+                            <div class="item-cart"></div>
+                        </div>
+                        <div class="picture" id="pic-cat-nine">
+                        </div>
+                        <h3>Galaxy</h3>
+                        <span class="model">Model : Kwadrat</span>
+                        <span class="price">89.90</span><span class="currency">PLN</span>
+                    </div>
+                </div>
+            </div>
+            <div id="pagination">
+                <p>
+                    <a href="#">
+                        &lt;&lt; Poprzednia strona</a> /
+                            <a href="#">Następna strona >></a></p>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div id="contact">
+            <p>738 892 973</p>
+            <p>sklep@purrlove.com</p>
+            <p>ul. Szewczyka 1/6 84-300 Lębork</p>
+        </div>
+        <div id="pages">
+            <p><a href="static-complaints.html">Reklamacje i zwroty</a> /
+                <a href="static-rules.html">Regulamin sklepu</a> /
+                <a href="static-payment.html">Płatność i dostawa</a></p>
+        </div>
+    </footer>
+</div>
+  );
+}
+
+export default Categories;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Route } from "wouter";
 import Home from './Home';
 import About from './About';
+import Categories from './Categories';
 
 import './scss/base.scss';
 
 const App = () => (
   <div>
+    <Route path="/categories"><Categories /></Route>
     <Route path="/about"><About /></Route>
     <Route path="/"><Home /></Route>
   </div>
