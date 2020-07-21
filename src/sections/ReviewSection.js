e8f939411f5e38336596b6097911d83f4c575336
deidrah <kumkwaczka@gmail.com>
2020-07-21 18:05:25 +0200

    Use react-slick in review section

--- src/sections/ReviewSection.js
+++ src/sections/ReviewSection.js
@@ -1,58 +1,80 @@
 import React from 'react';
+import Slider from "react-slick";
 import SectionHeading from '../components/SectionHeading'
+import SliderArrow from '../components/SliderArrow';
+
+
 
 function ReviewSection() {
-  return (
-        <div id="reviews">
-           <SectionHeading>Zobacz kto nas pokochał</SectionHeading>
-            <div class="owl-carousel">
-                <div class="rev-slide">
-                    <img src="img/zdj.png" alt="" />
-                <div class="points">
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_transp.png" alt="" />
-                </div>
-                <div class="rev-descr">
-                    <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
-                    <p>Mela, 22.05.2017</p>
-                </div>
-                </div>
-                <div class="rev-slide">
-                    <img src="img/zdj.png" alt="" />
-                <div class="points">
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_transp.png" alt="" />
-                </div>
-                <div class="rev-descr">
-                    <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
-                    <p>Mela, 22.05.2017</p>
-                </div>
-                </div>
-                <div class="rev-slide">
-                    <img src="img/zdj.png" alt="" />
-                <div class="points">
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_full.png" alt="" />
-                    <img src="img/paw_transp.png" alt="" />
-                </div>
-                <div class="rev-descr">
-                    <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
-                    <p>Mela, 22.05.2017</p>
-                </div>
-                </div>
-                
-            </div>
-            
-        </div>
+  var settings = {
+    autoplay: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SliderArrow direction="next" />,
+    prevArrow: <SliderArrow direction="prev" />
+  };
 
+  return (
+    <div id="reviews">
+      <SectionHeading>Zobacz kto nas pokochał</SectionHeading>
+      <Slider className="slider" {...settings}>
+        <div>
+          <div class="rev-slide">
+            <img src="img/zdj.png" alt="" />
+            <div class="points">
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_transp.png" alt="" />
+            </div>
+            <div class="rev-descr">
+              <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris
+                ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
+              <p>Mela, 22.05.2017</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="rev-slide">
+            <img src="img/zdj.png" alt="" />
+            <div class="points">
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_transp.png" alt="" />
+            </div>
+            <div class="rev-descr">
+              <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris
+                ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
+              <p>Mela, 22.05.2017</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="rev-slide">
+            <img src="img/zdj.png" alt="" />
+            <div class="points">
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_full.png" alt="" />
+              <img src="img/paw_transp.png" alt="" />
+            </div>
+            <div class="rev-descr">
+              <p><span>"</span>Cras non quam ultricesue nisi. Nunc sodales ultrat,<br /> non porttitor mauris
+                ornare non.<br /> Donec henderit vel velit ac mollis.<span>"</span></p>
+              <p>Mela, 22.05.2017</p>
+            </div>
+          </div>
+        </div>
+      </Slider>
+    </div>
   );
 }
 
