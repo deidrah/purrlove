9e0e98effc8637f386990bf6ff8fdaee691c81bb
deidrah <kumkwaczka@gmail.com>
2020-07-23 17:57:11 +0200

    Extract product component

---
+++ src/components/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Product(props) {
+  return (
+    <div class="item">
+      <div class="action-item">
+        <div class="item-cart"></div>
+      </div>
+      <div class="picture" id="pic-cat-seven"></div>
+      <h3>{props.name}</h3>
+      <span class="model">Model : {props.model}</span>
+      <span class="price">{props.price}</span><span class="currency">PLN</span>
+    </div>
+  );
+}
+
+export default Product;
