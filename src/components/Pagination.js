ec4103b678f89d6681732bf39059b630b3345a22
deidrah <kumkwaczka@gmail.com>
2020-07-23 17:57:04 +0200

    Extract pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Pagination() {
+  return (
+    <div id="pagination">
+      <p>
+        <a href="#">&lt;&lt; Poprzednia strona</a> /
+        <a href="#">Następna strona &gt;&gt;</a>
+      </p>
+    </div>
+  );
+}
+
+export default Pagination;
